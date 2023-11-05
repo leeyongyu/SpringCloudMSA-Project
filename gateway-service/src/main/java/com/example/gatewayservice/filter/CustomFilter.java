0f151f6851b7a84684d2b085dbe41d7f509e2da7
yonggyu2 <jmk154@naver.com>
2023-11-05 17:02:07 +0900

    2023.11.05 First Service Commit

---
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/CustomFilter.java
@@ -0,0 +1,40 @@
+package com.example.gatewayservice.filter;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+import org.springframework.http.server.reactive.ServerHttpRequest; // Webflux 지원
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+
+@Slf4j
+@Component
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+    public CustomFilter(){
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(CustomFilter.Config config){
+        // Pre Filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custom PRE filter : request id -> {}",request.getId());
+
+            //Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() ->{
+                log.info("Custom POST filter : resonse code ->{}",response.getStatusCode());
+            }));
+        };
+    }
+
+    public static class Config{
+
+    }
+}
