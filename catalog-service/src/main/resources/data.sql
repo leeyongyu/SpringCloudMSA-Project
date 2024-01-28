d0d4e61a76f1bc2e534002c84a403e3ffa7f2301
yonggyu2 <jmk154@naver.com>
2024-01-28 16:11:38 +0900

    2024.01.28 CatalogService commit

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-001','berlin',100,1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-002','apple',110,2000);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CATALOG-003','banana',80,800);
